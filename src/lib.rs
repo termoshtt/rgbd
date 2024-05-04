0150ef34acd6a3e7deb9b9fdee05ec30e94559bb
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-05 02:06:10 +0900

    WIP: Download instances from the GBD benchmark database

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,33 @@
-use std::path::PathBuf;
+//! Unofficial Rust client for [GBD Benchmark Database](https://github.com/Udopia/gbd), [A Problem Meta-Data Library for Research in SAT](https://easychair.org/publications/paper/jQXv)
+//!
+//! This crate downloads instance lists and actual benchmark instances from <https://benchmark-database.de/> on-demand, and caches them locally.
+//!
 
-pub fn data_root() -> PathBuf {
+use anyhow::Result;
+use std::path::PathBuf;
+use url::Url;
+
+/// The root directory where the database is cached
+pub fn cache_dir() -> PathBuf {
+    directories::ProjectDirs::from("", "", "rgbd")
+        .unwrap()
+        .cache_dir()
+        .to_owned()
+}
+
+pub struct Digest(String);
+
+impl Digest {
+    pub fn as_file_url(&self) -> Url {
+        Url::parse(&format!("https://benchmark-database.de/file/{}", self.0)).unwrap()
+    }
+}
+
+pub fn all_instances() -> Result<Vec<Digest>> {
+    todo!()
+}
+
+/// Get a list of instances for a given track
+pub fn get_track(track: &str) -> Result<Vec<Digest>> {
     todo!()
 }
