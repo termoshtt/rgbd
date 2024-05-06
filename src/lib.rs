ea5d652efac6d7118494cc7a4825d00de1bc439b
Toshiki Teramura <toshiki.teramura@gmail.com>
2024-05-06 12:26:38 +0900

    Split digest module into a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,31 @@
 //!
 //! This crate downloads instance lists and actual benchmark instances from <https://benchmark-database.de/> on-demand, and caches them locally.
 //!
+//! Examples
+//! ---------
+//!
+//! Get a benchmark instance of main track of SAT Competition 2023
+//!
+//! ```rust
+//! use rgbd::get_track;
+//!
+//! let instances = get_track("main_2023", false).unwrap();
+//! let cnf = instances[14].read().unwrap();
+//! assert_eq!(cnf.num_variables, 45);
+//! assert_eq!(cnf.num_clauses, 376);
+//! ```
 
+mod digest;
 mod parse;
 
+pub use digest::*;
 pub use parse::*;
 
 use anyhow::{Context, Result};
 use std::{fs, path::PathBuf};
 use url::Url;
 
-const BASE_URL: &str = "https://benchmark-database.de/";
+const BASE_URL: &str = "https://benchmark-database.de";
 
 /// The root directory where the database is cached
 pub fn cache_dir() -> PathBuf {
@@ -19,47 +34,6 @@ pub fn cache_dir() -> PathBuf {
         .unwrap()
         .cache_dir()
         .to_owned()
-}
-
-/// Digest (hash value) of a benchmark instance
-pub struct Digest(String);
-
-impl Digest {
-    pub fn as_file_url(&self) -> Url {
-        Url::parse(&format!("{BASE_URL}/file/{}", self.0)).unwrap()
-    }
-
-    pub fn from_url(url: &Url) -> Result<Self> {
-        let digest = url
-            .path_segments()
-            .context("URL is cannot be a base")?
-            .last()
-            .context("URL does not have path")?
-            .to_string();
-        Ok(Self(digest))
-    }
-
-    fn read_bytes(&self) -> Result<Vec<u8>> {
-        let cache = cache_dir().join("file").join(&self.0);
-        if cache.exists() {
-            Ok(fs::read(&cache)?)
-        } else {
-            let req = ureq::get(self.as_file_url().as_ref());
-            log::info!("GET {}", req.url());
-            let mut response = req.call()?.into_reader();
-            let mut buf = Vec::new();
-            response.read_to_end(&mut buf)?;
-            fs::create_dir_all(cache.parent().unwrap())?;
-            fs::write(&cache, &buf)?;
-            Ok(buf)
-        }
-    }
-
-    pub fn read(&self) -> Result<CNF> {
-        let bytes = self.read_bytes()?;
-        let decoder = xz2::read::XzDecoder::new(&bytes[..]);
-        CNF::from_dimacs_format(decoder).context("Failed to parse CNF")
-    }
 }
 
 /// Get a list of instances for a given track
